fd2cdba06313eec7e85a74d3c4b2c44ee7ed50ed
Sai Srimat <srimat@umail.ucsb.edu>
2017-12-01 15:38:41 -0800

    Trader edit: Constructor and menu

--- Trader.java
+++ Trader.java
@@ -24,66 +24,80 @@ public class Trader
 	
 	
 	//Constructor
-	public Trader(String name)
+	public Trader(String username)
 	{
-		// try
-		// {
-		// 	Class.forName("com.mysql.jdbc.Driver");  
-  //   		Connection con=DriverManager.getConnection("jdbc:mysql://cs174a.engr.ucsb.edu:3306/srimatDB","srimat","504");
-		// 	String query = "INSERT INTO Customer_Profile (customerId,name,username,password,STATE,Phone,Email,taxId) VALUES (NULL,?,?,?,?,NULL,NULL,?)";
-		// 	PreparedStatement stmt=con.prepareStatement(query);
-		// 	stmt.setString(1, "Test");
-		// 	stmt.setString(2, "TestUser");
-		// 	stmt.setString(3, "TestPwd");
-		// 	stmt.setString(4, "CA");
-		// 	stmt.setInt(5, 123);
-		// 	stmt.executeUpdate();
-		// 	stmt.close();
-  //       	//ResultSet rs=stmt.executeUpdate();
-		// }
-		// catch(Exception e)
-		// {
-		// 	System.out.println(e);
-		// }
-		this.name = name;
-	}
+		this.username = username;
 
 
-	public void setUserName(String user)
-	{
-		username = user;
-	}
-
-	public void setPassword(String pwd)
-	{
-		password = pwd;
-	}
-
-	public void setState(String st)
-	{
-		state = st;
-	}
-
-	public void setPhone(String ph)
-	{
-		phone = ph;
-	}
-
-	public void setEmail(String em)
-	{
-		email = em;
-	}
-
-	public void setTaxId(int tax)
-	{
-		taxid = tax;
-	}
-
-	/*public void Deposit()
-	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");  
+    		Connection con=DriverManager.getConnection(HOST,USER,PWD);
+			String query = "select * from Customer_Profile where username = ?";
+			PreparedStatement stmt=con.prepareStatement(query);
+			stmt.setString(1, username);
+        	ResultSet rs=stmt.executeQuery();
+        	while(rs.next())
+        	{
+        		name = rs.getString("name");
+        		password = rs.getString("password");
+        		state = rs.getString("STATE");
+        		phone = rs.getString("Phone");
+        		email = rs.getString("Email");
+        		taxid = rs.getInt("taxId");
+        	}
+        	stmt.close();
+        	con.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
 
 	}
 
+	// public void Deposit(int taxid, double amount)
+	// {
+	// 	Connection connection = null;
+	//     Statement statement = null;
+
+	// 	try
+	// 	{
+	// 		Class.forName("com.mysql.jdbc.Driver");
+	// 	} 
+	// 	catch(ClassNotFoundException e){
+	// 		e.printStackTrace();
+	// 	}
+
+	// 	try
+	// 	{
+	// 		connection=DriverManager.getConnection(HOST,USER,PWD);
+	// 		String QUERY1 = "update Market_Accounts set balance = ";
+	// 		String QUERY2 = "";
+
+	// 		PreparedStatement myQuery = connection.prepareStatement(QUERY);
+	// 		myQuery.setString(1, username);
+	// 		myQuery.setString(2, password);
+	//         ResultSet resultSet = myQuery.executeQuery();
+	//         boolean empty = true;
+	//         while (resultSet.next()) {
+	//          	empty = false;
+	//             }
+	//         if(empty){
+	//         	System.out.println("Incorrect username or password. Please try again.");
+	//         }
+	//         else{
+	//         	System.out.println("Login successful! Welcome to your Trader Portal!");
+	//         }
+	//     }
+	//     catch(SQLException e)
+	//     {
+	//     	e.printStackTrace();
+	//     }
+
+
+	// }
+	/*
 	public void Withdrawal()
 	{
 		
@@ -99,7 +113,12 @@ public class Trader
 		
 	}
 
-	public int showBalance()
+	public double getBalance()
+	{
+	
+	}
+
+	public double showBalance()
 	{
 
 	}
@@ -119,61 +138,86 @@ public class Trader
 
 	}*/
 
-	public static void main(String []args) 
+	public void menu() 
 	{
+		// while(True){
 		
-		// try
-		// {
-		// 	Class.forName("com.mysql.jdbc.Driver");  
-  //   		Connection con=DriverManager.getConnection(HOST,USER,PWD);
-  //   	}
-  //   	catch(Exception e)
-  //   	{
-  //   		System.out.println(e);
-  //   	}
-		Scanner reader = new Scanner(System.in);
-		String []inputs = new String[2];
-		System.out.println("Welcome! Please provide a valid username: ");
-      	inputs[0] = reader.nextLine();
-      	System.out.println("and now a valid password: ");
-  		inputs[1] = reader.nextLine();
-  		String username = inputs[0];
-  		String password = inputs[1];
-  		System.out.println(username + " " + password);
+		// 	Scanner reader = new Scanner(System.in);
+		// 	System.out.println("Welcome to the Trader Interface!");
+		// 	System.out.println("Do you wish to: ");
+		// 	System.out.println("(1) Make a Deposit");
+		// 	System.out.println("(2) Make a Withdrawal");
+		// 	System.out.println("(3) Buy Stocks");
+		// 	System.out.println("(4) Sell Stocks");
+		// 	System.out.println("(5) Show balance of your Market Account");
+		//   	String answer = reader.nextLine();
 
-		Connection connection = null;
-        Statement statement = null;
+		//   	switch(answer){
+		//   		case '1':
+		//   			System.out.println("How much money would you like to deposit?");
+		//   			String temp = reader.nextLine();
+		//   			Double amount = Double.parseDouble(temp);
+		//   			if(amount < 0){
+		//   				System.out.println("Please provide a dollar amount greater than 0");
+		//   				amount = reader.nextLine();
+		//   				Deposit(amount);
+		//   			}
+		//   			else{
+		//   				Deposit(amount);
+		//   			}
+		//   		case '2':
+		//   			Withdrawal();
+		//   		case '3':
+		//   			Buy();
+		//  		default:
+		//  			System.out.println("The option you chose is not listed. Please provide a valid option");
+		//  			break;
+		//   	}
 
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-		} 
-		catch(ClassNotFoundException e){
-			e.printStackTrace();
-		}
-		
-		try{
-			connection=DriverManager.getConnection(HOST,USER,PWD);
-			String QUERY = "select username, password from Customer_Profile where username=? and password=?";
+		// 	Scanner reader = new Scanner(System.in);
+		// 	String []inputs = new String[2];
+		// 	System.out.println("Welcome! Please provide a valid username: ");
+		//   	inputs[0] = reader.nextLine();
+		//   	System.out.println("and now a valid password: ");
+		// 	inputs[1] = reader.nextLine();
+		// 	String username = inputs[0];
+		// 	String password = inputs[1];
+		// 	System.out.println(username + " " + password);
 
-			PreparedStatement myQuery = connection.prepareStatement(QUERY);
-			myQuery.setString(1, username);
-			myQuery.setString(2, password);
-	        ResultSet resultSet = myQuery.executeQuery();
-	        boolean empty = true;
-	        while (resultSet.next()) {
-	         	empty = false;
-	            }
-	        if(empty){
-	        	System.out.println("Incorrect username or password. Please try again.");
-	        }
-	        else{
-	        	System.out.println("Login successful! Welcome to your Trader Portal!");
-	        }
-        }
-        catch(SQLException e)
-        {
-        	e.printStackTrace();
-        }
+		// 	Connection connection = null;
+		//     Statement statement = null;
+
+		// 	try{
+		// 		Class.forName("com.mysql.jdbc.Driver");
+		// 	} 
+		// 	catch(ClassNotFoundException e){
+		// 		e.printStackTrace();
+		// 	}
+			
+			// try{
+			// 	connection=DriverManager.getConnection(HOST,USER,PWD);
+			// 	String QUERY = "select username, password from Customer_Profile where username=? and password=?";
+
+			// 	PreparedStatement myQuery = connection.prepareStatement(QUERY);
+			// 	myQuery.setString(1, username);
+			// 	myQuery.setString(2, password);
+		 //        ResultSet resultSet = myQuery.executeQuery();
+		 //        boolean empty = true;
+		 //        while (resultSet.next()) {
+		 //         	empty = false;
+		 //            }
+		 //        if(empty){
+		 //        	System.out.println("Incorrect username or password. Please try again.");
+		 //        }
+		 //        else{
+		 //        	System.out.println("Login successful! Welcome to your Trader Portal!");
+		 //        }
+		 //    }
+		 //    catch(SQLException e)
+		 //    {
+		 //    	e.printStackTrace();
+		 //    }
+		//}
 
      //  /* Object creation */
     	// Trader trader = new Trader("Sai");
