f46487e70ea4522b95aaa249ee26ff3d52eda21d
Ziheng Song <zihengsong@169-231-158-53.wireless.ucsb.edu>
2017-12-01 15:44:33 -0800

    main revised

--- Main.java
+++ Main.java
@@ -1,22 +1,54 @@
 import java.io.*;
-import DBPackage.*;
-
-
+import java.util.Scanner;
 
 public class Main{
 	public static void main(String [ ] args)
 	{
-		Trader trader = new Trader("Sai");
+		// Trader trader = new Trader("Sai");
 
-		trader.setUserName("srimat");
-	    trader.setPassword("1234");
-	    trader.setState("CA");
-	    trader.setPhone("6504549782");
+		// trader.setUserName("srimat");
+	 //    trader.setPassword("1234");
+	 //    trader.setState("CA");
+	 //    trader.setPhone("6504549782");
 
-	    System.out.println("Name:"+ trader.name );
-	    System.out.println("Username:"+ trader.username );
-	    System.out.println("Password:" + trader.password );
-	    System.out.println("State:" + trader.state);
-	  	System.out.println("Phone:" + trader.phone);
+	 //    System.out.println("Name:"+ trader.name );
+	 //    System.out.println("Username:"+ trader.username );
+	 //    System.out.println("Password:" + trader.password );
+	 //    System.out.println("State:" + trader.state);
+	 //  	System.out.println("Phone:" + trader.phone);
+
+		System.out.println("Welcome to StarsRus Brokerage!");
+		System.out.println("Are you a trader or a manager? Enter t for trader and m for manager. Enter q to exit.");
+		
+
+		while(true) {
+			Scanner reader = new Scanner(System.in);
+			String role = reader.nextLine();
+
+			if(role.length() != 1) {
+				System.out.println(role);
+				System.out.println("Too many characters. Enter t or m to log into your account.");
+			}
+			else{
+				//System.out.println(role);
+				if (role.equals("t")) {
+					System.out.println("Successful!");
+					//Trader trader = new Trader();
+					break;
+				}
+				if (role.equals("m")) {
+					//Manager mnager = new Manager();
+					break;
+				}
+				if (role.equals("q")) {
+					break;
+				}
+				else {
+					System.out.println("Incorrect input. Enter t or m to log into your account.");
+				}
+			}
+		}
+
+		System.exit(0);
 	}
 }
