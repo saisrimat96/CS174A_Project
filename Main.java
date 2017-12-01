b8e2c8201cc164caae43ee2bca7f04208e27c1e1
Sai Srimat <srimat@umail.ucsb.edu>
2017-12-01 15:40:58 -0800

    minor Main changes

--- Main.java
+++ Main.java
@@ -1,22 +1,12 @@
 import java.io.*;
-import DBPackage.*;
+
 
 
 
 public class Main{
 	public static void main(String [ ] args)
 	{
-		Trader trader = new Trader("Sai");
-
-		trader.setUserName("srimat");
-	    trader.setPassword("1234");
-	    trader.setState("CA");
-	    trader.setPhone("6504549782");
-
-	    System.out.println("Name:"+ trader.name );
-	    System.out.println("Username:"+ trader.username );
-	    System.out.println("Password:" + trader.password );
-	    System.out.println("State:" + trader.state);
-	  	System.out.println("Phone:" + trader.phone);
+		Trader trader = new Trader("alfred");
+		//trader.menu();
 	}
 }
