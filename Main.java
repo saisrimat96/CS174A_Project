7191238d1a9d78406395e7d0fd67771d1bca17a6
Sai Srimat <srimat@umail.ucsb.edu>
2017-12-02 15:53:06 -0800

    Test Main

--- Main.java
+++ Main.java
@@ -6,7 +6,13 @@ import java.io.*;
 public class Main{
 	public static void main(String [ ] args)
 	{
-		Trader trader = new Trader("alfred");
-		//trader.menu();
+		Trader trader = new Trader("sai");
+		//trader.Deposit(1022, 300.00);
+		//trader.Withdrawal(100, 200.00);
+		//trader.showBalance();
+		//trader.showTransactions();
+		//trader.showStockProf("SMD");
+		//trader.Buy(100, 3, "STC");
+		trader.menu();
 	}
 }
