9acd5d551521dc21688ac62579f9a90f0f5a3955
Ziheng Song <zihengsong@128-111-64-46.vpn.ucsb.edu>
2017-12-02 17:06:47 -0800

    Manager revised

--- Manager.java
+++ Manager.java
@@ -21,7 +21,33 @@ public class Manager
 	public Manager(String username)
 	{
 		this.username = username;
-		System.out.println(username);
+	}
+
+	public void menu()
+	{
+		while(True){
+			System.out.println("Welcome to the Manager Interface!");
+			System.out.println("Do you wish to: ");
+			System.out.println("(1) Add Interest for Market Accounts");
+			System.out.println("(2) Generate Monthly Statement for a particular customer");
+			System.out.println("(3) List Active Customers");
+			System.out.println("(4) Generate Government Drug & Tax Evasion Report(DTER)");
+			System.out.println("(5) Generate Customer Report");
+			System.out.println("(6) Delete Transactions");
+			Scanner reader = new Scanner(System.in);
+		  	String answer = reader.nextLine();
+
+		  	switch(answer){
+		  		case '1':
+		  			addInterest();
+		  		case '2':
+		  			Withdrawal();
+		  		case '3':
+		  			Buy();
+		 		default:
+		 			System.out.println("The option you chose is not listed. Please provide a valid option");
+		 			break;
+		  	}
 	}
 
 	public void addInterest()
@@ -43,75 +69,29 @@ public class Manager
 		}
 	}
 
-	// public void setUserName()
+	// public void genStatement()
 	// {
 
 	// }
 
-	// public void setPassword()
+	// public void listActive()
 	// {
 
 	// }
 
-	// public void setAddress()
+	// public void genDTER()
 	// {
 
 	// }
 
-	// public void setState()
+	// public void report()
 	// {
 
 	// }
 
-	// public void setPhone()
+	// public void deleteTran()
 	// {
 
 	// }
-
-	// public void setEmail()
-	// {
-
-	// }
-
-	// public void setTaxId()
-	// {
-
-	// }
-	public void menu()
-	{
-		// while(True){
-		
-		// 	Scanner reader = new Scanner(System.in);
-		// 	System.out.println("Welcome to the Trader Interface!");
-		// 	System.out.println("Do you wish to: ");
-		// 	System.out.println("(1) Make a Deposit");
-		// 	System.out.println("(2) Make a Withdrawal");
-		// 	System.out.println("(3) Buy Stocks");
-		// 	System.out.println("(4) Sell Stocks");
-		// 	System.out.println("(5) Show balance of your Market Account");
-		//   	String answer = reader.nextLine();
-
-		//   	switch(answer){
-		//   		case '1':
-		//   			System.out.println("How much money would you like to deposit?");
-		//   			String temp = reader.nextLine();
-		//   			Double amount = Double.parseDouble(temp);
-		//   			if(amount < 0){
-		//   				System.out.println("Please provide a dollar amount greater than 0");
-		//   				amount = reader.nextLine();
-		//   				Deposit(amount);
-		//   			}
-		//   			else{
-		//   				Deposit(amount);
-		//   			}
-		//   		case '2':
-		//   			Withdrawal();
-		//   		case '3':
-		//   			Buy();
-		//  		default:
-		//  			System.out.println("The option you chose is not listed. Please provide a valid option");
-		//  			break;
-		//   	}
-	}
 
 }
