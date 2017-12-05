b42baed3ae32b2bf37516c9743d15fd56d5db6b2
Ziheng Song <zihengsong@128-111-64-134.vpn.ucsb.edu>
2017-12-05 09:21:02 -0800

    add market class, revise main

--- Manager.java
+++ Manager.java
@@ -10,55 +10,63 @@ import java.util.Scanner;
 
 public class Manager
 {
-	String username;
+	private String username;
+	Connection connection = null;
 
 	public static final String HOST = "jdbc:mysql://cs174a.engr.ucsb.edu:3306/srimatDB";
     public static final String USER = "srimat";
     public static final String PWD = "504";
 	
-	
 	//Constructor
 	public Manager(String username)
 	{
 		this.username = username;
-	}
 
-	public void addInterest()
-	{
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
-    		Connection con = DriverManager.getConnection(HOST,USER,PWD);
-			String query = "update Market_Accounts SET balance = (balance * 1.03)";
-        	PreparedStatement stmt = con.prepareStatement(query);
-        	stmt.executeUpdate();
-        	System.out.println("You have added interest for all customers.");
-        	stmt.close();
-        	con.close();
-		}
+    		connection = DriverManager.getConnection(HOST,USER,PWD);
+		} 
 		catch(Exception e)
 		{
 			System.out.println(e);
 		}
 	}
 
-	public void genStatement(String name)
+	public void closeConnection()
 	{
-		Connection connection = null;
-		int taxid = 0;
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			connection.close();
 		} 
-		catch(ClassNotFoundException e)
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+	public void addInterest()
+	{
+		try
+		{
+			String query = "update Market_Accounts SET balance = (balance * 1.03)";
+        	PreparedStatement stmt = connection.prepareStatement(query);
+        	stmt.executeUpdate();
+        	System.out.println("You have added interest for all customers.");
+        	stmt.close();
+		}
+		catch(SQLException e)
 		{
 			e.printStackTrace();
 		}
+	}
 
+	public void genStatement(String name)
+	{
+		int taxid = 0;
 		try
 		{
 			//Find the taxId and email of a given customer name from Customer_Profile
-			connection = DriverManager.getConnection(HOST,USER,PWD);
 			Statement stmt = connection.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT Email, taxId FROM Customer_Profile WHERE name = '" + name + "'");
 			if(rs.next()){
@@ -71,22 +79,21 @@ public class Manager
 
 	    	//List all transactions
 			Statement stmt2 = connection.createStatement();
-			rs = stmt2.executeQuery("SELECT * FROM Transactions WHERE taxId=" + taxid);
+			rs = stmt2.executeQuery("SELECT * FROM Transactions WHERE taxId = " + taxid);
 			while(rs.next()){
 				int t1 = rs.getInt("transactionsId");
 				String t2 = rs.getString("date");
 				String t3 = rs.getString("type");
 				Double t4 = rs.getDouble("amount");
 				int t5 = rs.getInt("numShares");
-				int t6 = rs.getInt("stockId");
+				int t6 = rs.getInt("symbol");
 				int t7 = rs.getInt("taxId");
 				System.out.println("Trans History for: " + name);
-				System.out.println("transactionsID: " + t1 + ", date: " + t2 + ", type: " + t3 + ", amount: " + t4 + ", numShares: " + t5 + ", stockId: " + t6 + ", taxId: " + t7);
+				System.out.println("transactionsID: " + t1 + ", date: " + t2 + ", type: " + t3 + ", amount: " + t4 + ", numShares: " + t5 + ", symbol: " + t6 + ", taxId: " + t7);
 			}
 
 			rs.close();
 			stmt2.close();
-			connection.close();
 		}
 		catch(SQLException e)
 	    {
@@ -138,10 +145,10 @@ public class Manager
 	// 			String t3 = rs2.getString("type");
 	// 			Double t4 = rs2.getDouble("amount");
 	// 			int t5 = rs2.getInt("numShares");
-	// 			int t6 = rs2.getInt("stockId");
+	// 			int t6 = rs2.getInt("symbol");
 	// 			int t7 = rs2.getInt("taxId");
 	// 			System.out.println("Trans History for: " + name);
-	// 			System.out.println("transactionsID: " + t1 + ", date: " + t2 + ", type: " + t3 + ", amount: " + t4 + ", numShares: " + t5 + ", stockId: " + t6 + ", taxId: " + t7);
+	// 			System.out.println("transactionsID: " + t1 + ", date: " + t2 + ", type: " + t3 + ", amount: " + t4 + ", numShares: " + t5 + ", symbol: " + t6 + ", taxId: " + t7);
 	// 		}
 
 	// 		rs2.close();
@@ -161,21 +168,10 @@ public class Manager
 
 	public void report(String name)
 	{
-		Connection connection = null;
 		int taxid = 0;
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
-		} 
-		catch(ClassNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-
-		try
-		{
 			//Find the taxId of a given customer name from Customer_Profile
-			connection = DriverManager.getConnection(HOST,USER,PWD);
 			Statement stmt = connection.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT taxId FROM Customer_Profile WHERE name = '" + name + "'");
 			if(rs.next()){
@@ -186,7 +182,7 @@ public class Manager
 
 	    	//List market account associated with the customer
 			Statement stmt2 = connection.createStatement();
-			rs = stmt2.executeQuery("SELECT balance FROM Market_Accounts WHERE taxId=" + taxid);
+			rs = stmt2.executeQuery("SELECT balance FROM Market_Accounts WHERE taxId = " + taxid);
 			if(rs.next()){
 				Double balance = rs.getDouble(1);
 				System.out.println("Market Account information for: " + name + ", taxId: " + taxid);
@@ -199,7 +195,7 @@ public class Manager
 	    	Double balance2 = 0.0;
 	    	String symbol = "";
 			Statement stmt3 = connection.createStatement();
-			rs = stmt3.executeQuery("SELECT balance, symbol FROM Stock_Accounts WHERE taxId=" + taxid);
+			rs = stmt3.executeQuery("SELECT balance, symbol FROM Stock_Accounts WHERE taxId = " + taxid);
 			while(rs.next()){
 				balance2 = rs.getDouble(1);
 				symbol = rs.getString(2);
@@ -220,18 +216,15 @@ public class Manager
 	{
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
-    		Connection con = DriverManager.getConnection(HOST,USER,PWD);
 			String query = "delete from Transactions";
-        	PreparedStatement stmt = con.prepareStatement(query);
+        	PreparedStatement stmt = connection.prepareStatement(query);
         	stmt.executeUpdate();
         	System.out.println("You have deleted transactions from all accounts.");
         	stmt.close();
-        	con.close();
 		}
-		catch(Exception e)
+		catch(SQLException e)
 		{
-			System.out.println(e);
+			e.printStackTrace();
 		}
 	}
 
@@ -246,11 +239,10 @@ public class Manager
 			System.out.println("(4) Generate Government Drug & Tax Evasion Report(DTER)");
 			System.out.println("(5) Generate Customer Report");
 			System.out.println("(6) Delete Transactions from all Accounts");
+			System.out.println("(7) Quit Manager Interface");
 
 			Scanner reader = new Scanner(System.in);
 		  	String answer = reader.nextLine();
-		  	Connection connection = null;
-			Statement statement = null;
 
 		  	switch(answer){
 		  		case "1":
@@ -273,19 +265,6 @@ public class Manager
 		  			System.out.println("\nWhich customer's statement would you like to generate?");
 		  			String name2 = reader.nextLine();
 		  			genStatement(name2);
-		  			while(true) {
-		  				System.out.println("\nDo you wish to go back to manager menu? Enter 'y' for yes or 'q' to exit Manager Interface.");
-		  				String answer2 = reader.nextLine();
-		  				if(answer2.equals("q")) {
-		  					System.exit(0);
-		  				}
-		  				else if(answer2.equals("y")) {
-		  					break;
-		  				}
-		  				else{
-		  					System.out.println("\nThe option you chose is not listed. Please provide a valid option.");
-		  				}
-		  			}
 		  			break;
 		  		// case "3":
 		  		// 	listActive();
@@ -295,19 +274,6 @@ public class Manager
 		  			System.out.println("\nWhich customer's report would you like to check?");
 		  			String name5 = reader.nextLine();
 		  			report(name5);
-		  			while(true) {
-		  				System.out.println("\nDo you wish to go back to manager menu? Enter 'y' for yes or 'q' to exit Manager Interface.");
-		  				String answer2 = reader.nextLine();
-		  				if(answer2.equals("q")) {
-		  					System.exit(0);
-		  				}
-		  				else if(answer2.equals("y")) {
-		  					break;
-		  				}
-		  				else{
-		  					System.out.println("\nThe option you chose is not listed. Please provide a valid option.");
-		  				}
-		  			}
 		  			break;
 		  		case "6":
 		  			while(true) {
@@ -325,10 +291,26 @@ public class Manager
 			  			}
 			  		}
 		  			break;
+		  		case "7":
+		  			System.exit(0);
 		 		default:
-		 			System.out.println("\nThe option you chose is not listed. Please provide a valid option");
+		 			System.out.println("\nThe option you chose is not listed. Please provide a valid option.");
 		 			break;
 		  	}
+
+		  	while(true) {
+		  		System.out.println("\nDo you wish to go back to Manager Interface? Enter 'y' for yes or 'q' to exit Manager Interface.");
+		  		String answer2 = reader.nextLine();
+		  		if(answer2.equals("q")) {
+		  			System.exit(0);
+		  		}
+		  		else if(answer2.equals("y")) {
+		  			break;
+				}
+  				else{
+  					System.out.println("\nThe option you chose is not listed. Please provide a valid option.");
+  				}
+  			}
 		}
 	}
 }
